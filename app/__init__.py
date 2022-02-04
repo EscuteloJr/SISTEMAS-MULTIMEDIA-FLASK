f1405fc92596e44d847e11feead69353223a4bfc
EscuteloJr <mateusescutelo@gmail.com>
2022-02-04 12:25:51 +0100

    df

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/creche'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 app.secret_key = "root_123"
 
